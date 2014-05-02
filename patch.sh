5ae8e5d24ccb11496a865f16878f7a4f720e8cbc
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-05-03 00:47:26 +0200

    Changed ceres and its dependencies to proper External projects and dumped the old shell script

---
+++ patch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "### Patching ceres cmake ###"
+# Remove -Werror from cmake lists as clang outputs warnings for unused include paths.
+sed -i 's/-Werror/-Wall/g' CMakeLists.txt
+# Add gflags to linker list for ceres.
+sed -i 's/SET(CERES_LIBRARY_DEPENDENCIES ${GLOG_LIBRARIES})/SET(CERES_LIBRARY_DEPENDENCIES ${GLOG_LIBRARIES} ${GFLAGS_LIBRARIES})/g' internal/ceres/CMakeLists.txt
+
