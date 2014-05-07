2004735e2cef80a4a7bdce154a7072d5926ee4a6
Paul Furgale <paul.furgale@gmail.com>
2014-05-07 12:37:54 +0200

    Compile on osx

--- patch.sh
+++ patch.sh
@@ -2,7 +2,7 @@
 
 echo "### Patching ceres cmake ###"
 # Remove -Werror from cmake lists as clang outputs warnings for unused include paths.
-sed -i 's/-Werror/-Wall/g' CMakeLists.txt
+sed -i.bu 's/-Werror/-Wall/g' CMakeLists.txt
 # Add gflags to linker list for ceres.
-sed -i 's/SET(CERES_LIBRARY_DEPENDENCIES ${GLOG_LIBRARIES})/SET(CERES_LIBRARY_DEPENDENCIES ${GLOG_LIBRARIES} ${GFLAGS_LIBRARIES})/g' internal/ceres/CMakeLists.txt
+sed -i.bu 's/SET(CERES_LIBRARY_DEPENDENCIES ${GLOG_LIBRARIES})/SET(CERES_LIBRARY_DEPENDENCIES ${GLOG_LIBRARIES} ${GFLAGS_LIBRARIES})/g' internal/ceres/CMakeLists.txt
 
