0b85cb2e43223c02654375089f272cf231177f12
Simon Lynen <simon.lynen@mavt.ethz.ch>
2014-10-20 01:20:48 +0200

    Path iomp

--- patch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "### Patching ceres cmake ###"
-# Remove -Werror from cmake lists as clang outputs warnings for unused include paths.
-sed -i.bu 's/-Werror/-Wall/g' CMakeLists.txt
-# Add gflags to linker list for ceres.
-sed -i.bu 's/SET(CERES_LIBRARY_DEPENDENCIES ${GLOG_LIBRARIES})/SET(CERES_LIBRARY_DEPENDENCIES ${GLOG_LIBRARIES} ${GFLAGS_LIBRARIES})/g' internal/ceres/CMakeLists.txt
-
